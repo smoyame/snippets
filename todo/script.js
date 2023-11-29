6ea2ee33693be86f776282cbc89b4d6b9033f234
smoyamedina <smoyamedina@outlook.com>
2023-11-28 21:23:35 -0500

    Rearrange + factor mildly

--- todo/script.js
+++ todo/script.js
@@ -1,22 +1,6 @@
 // script.js
 let list = new Map();
 
-function move(target) {
-	let currentItem = target.parentElement.parentElement;
-	let pendingList = document.querySelector('.pending .todo-list');
-	let completedList = document.querySelector('.complete .todo-list');
-	let instance = list.get(currentItem.innerText)
-	if (target.checked) {
-		completedList.appendChild(currentItem)
-		list.set(currentItem.innerText, true);
-		localStorage.setItem(target.parentElement.parentElement.innerText, true)
-	} else {
-		pendingList.appendChild(currentItem)
-		list.set(currentItem.innerText, false);
-		localStorage.setItem(target.parentElement.parentElement.innerText, false)
-	}
-}
-
 function newLi() {
 	let li = document.createElement("li");
 	let label = document.createElement("label");
@@ -51,6 +35,21 @@ function add(text = '', complete = false) {
 		list.set(todoText, complete);
 		localStorage[todoText] = complete;
 		document.querySelector('#todo').value = ''
+	}
+}
+
+const pendingList = document.querySelector('.pending .todo-list');
+const completedList = document.querySelector('.complete .todo-list');
+function move(target) {
+	let currentItem = target.parentElement.parentElement;
+	if (target.checked) {
+		completedList.appendChild(currentItem)
+		list.set(currentItem.innerText, true);
+		localStorage.setItem(target.parentElement.parentElement.innerText, true)
+	} else {
+		pendingList.appendChild(currentItem)
+		list.set(currentItem.innerText, false);
+		localStorage.setItem(target.parentElement.parentElement.innerText, false)
 	}
 }
 
