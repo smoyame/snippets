d4a3c8d0233ba14f0782899b87653715b20f24b2
smoyamedina <smoyamedina@outlook.com>
2024-07-13 20:32:45 -0400

    Update + reveal HTML on click with getTimeStamp()

--- clipborn/script.js
+++ clipborn/script.js
@@ -1,4 +1,16 @@
-let lastUse = new Date()
-let lastUseDate = lastUse.toLocaleDateString('en-US')
-let lastUseTime = lastUse.toLocaleTimeString('en-US', { timeStyle: "short", hour12: false })
-let lastUseISO = lastUse.toISOString()
+let timestampGraf = document.querySelector('.timestamp')
+let timeElement = document.querySelector('#time')
+
+function getTimeStamp() {
+	let lastUse = new Date()
+	let lastUseDate = lastUse.toLocaleDateString('en-US')
+	let lastUseTime = lastUse.toLocaleTimeString('en-US', { timeStyle: "short", hour12: false })
+
+	timeElement.attributes.datetime.value = lastUse.toISOString()
+	timeElement.innerText = `${lastUseTime} on ${lastUseDate}`
+
+	if (!timestampGraf.style.opacity) {
+		timestampGraf.style.opacity = 1
+		timestampGraf.removeAttribute("aria-hidden")
+	}
+}
