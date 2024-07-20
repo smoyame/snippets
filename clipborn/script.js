68fef57dccf3354c159eb4369b06a147a87a5e98
smoyamedina <smoyamedina@outlook.com>
2024-07-19 20:50:47 -0400

    Successfully inserting new rows into table with basic clipboard MIME and content data.

--- clipborn/script.js
+++ clipborn/script.js
@@ -27,3 +27,53 @@ function updateTimeStamp() {
 		timestampGraf.removeAttribute("aria-hidden")
 	}
 }
+
+let table = document.querySelector('#output-table tbody')
+let origRow = document.querySelector('[data-table="row-template"]')
+const templateRow = origRow
+let genericRowContents = document.querySelector('[data-table="row-template"]').innerHTML
+
+function newRow(type, attr, content, index = "00") {
+	let timestamp = Date.now()
+	let newRow = templateRow.cloneNode(true)
+
+	newRow.setAttribute("data-table", "row")
+	newRow.setAttribute("data-added", timestamp)
+
+	newRow.querySelector('[data-column="index"]').innerText = index
+	newRow.querySelector('[data-column="data-type"]').innerText = type
+	newRow.querySelector('[data-column="attribute"]').innerText = attr
+	newRow.querySelector('[data-column="preview"] pre').innerText = content
+
+	if (templateRow) {
+		templateRow.remove()
+	}
+	table.appendChild(newRow)
+}
+
+
+if (navigator.clipboard) {
+	async function getClipList() {
+		try {
+			const items = await navigator.clipboard.read();
+			for (const clipboardItem of items) {
+				let decoder = new TextDecoder()
+				let selectedType = clipboardItem.types[0]
+
+				let blob = await clipboardItem.getType(selectedType);
+				let blobText = await blob.bytes()
+				let decodedText = decoder.decode(blobText)
+
+				newRow(selectedType, "ATTR", decodedText)
+				console.log(selectedType)
+			}
+		} catch (err) {
+			console.error(err.name, err.message);
+		}
+	}
+
+	document.querySelector('.fi-input').addEventListener('paste', () => {
+		updateTimeStamp()
+		getClipList()
+	})
+}
